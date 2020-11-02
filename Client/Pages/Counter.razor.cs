170f42ebdde5ba4a43283bee733d1723d26e0bbd
Ariel Molina <amolina@educaria.com>
2020-11-02 16:08:28 -0300

    Added Inherits in counter page

---
+++ Client/Pages/Counter.razor.cs
@@ -0,0 +1,27 @@
+﻿using BlazorDemo.Client.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.Client.Pages
+{
+    public class CounterBase : ComponentBase
+    {
+        protected int currentCount = 0;
+        
+        [Inject]
+        protected SingletonService Singleton { get; set; }
+
+        [Inject]
+        protected TransientService Transient { get; set; }
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+            Singleton.Value = currentCount;
+            Transient.Value = currentCount;
+        }
+    }
+}
