fb217ccaae82de6eeb457dc5975fc0776c3db0c9
Ariel Molina <amolina@educaria.com>
2020-11-02 12:32:27 -0300

    Added film list to index

---
+++ Shared/Models/Film.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorDemo.Shared.Models
+{
+    public class Film
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
