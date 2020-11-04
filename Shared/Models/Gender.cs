20b20bf5b942f470125262318ab1b3836b476fb4
Ariel Molina <amolina@educaria.com>
2020-11-04 18:15:49 -0300

    Added Create filter page

---
+++ Shared/Models/Gender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorDemo.Shared.Models
+{
+    public class Gender
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Name { get; set; }
+    }
+}
