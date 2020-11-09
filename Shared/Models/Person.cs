c212f6f1e6b421620e3075584521782133e07065
Ariel Molina <amolina@educaria.com>
2020-11-09 12:16:53 -0300

    Added characters finder when create or edit a film

--- Shared/Models/Person.cs
+++ Shared/Models/Person.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -14,5 +14,19 @@ namespace BlazorDemo.Shared.Models
         public string Picture { get; set; }
         [Required]
         public DateTime? Birthdate { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Person p2)
+            {
+                return Id == p2.Id;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
